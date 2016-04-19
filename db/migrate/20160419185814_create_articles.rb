91625145f6b7783d554b1b7130a969c9b95d58eb
juan267 <j.gomez267@gmail.com>
2016-04-19 13:58:29 -0500

    fix generator error

--- db/migrate/20160419185814_create_articles.rb
+++ db/migrate/20160419185814_create_articles.rb
@@ -1,7 +1,8 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.stringbody :title
+      t.string :title
+      t.text :body
 
       t.timestamps null: false
     end
