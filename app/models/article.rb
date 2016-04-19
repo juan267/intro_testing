5d38aa764d34322e87cb3b7e147c54b654a173c4
juan267 <j.gomez267@gmail.com>
2016-04-19 15:50:42 -0500

    fix capitalize test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true
+  validates :body, length: {minimum: 140 }
+
+  def capitalize_title
+    self.title = self.title.capitalize
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -10,14 +10,20 @@ class ArticleTest < ActiveSupport::TestCase
     assert_not article.save, 'Saved the article without a title'
   end
 
-  test 'should report an error' do
-    some_undefined_variable
-    assert true
-  end
+  # test 'should report an error' do
+  #   some_undefined_variable
+  #   assert true
+  # end
 
   test 'should not save article with a body less than 140 characters' do
     article = Article.new(title: "Hola")
     assert_not article.save, 'Saved article with less than 140 characters'
+  end
+
+  test 'capitalize should always leave the Title capitalized' do
+    article = Article.create(title: "hola")
+    article.capitalize_title
+    assert_equal("Hola", article.title)
   end
 
 end
